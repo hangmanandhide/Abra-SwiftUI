fa83c4f333b0b08ce8d8ee14002cf0c60f249eb0
HangmanAndHide <9014584+hangmanandhide@users.noreply.github.com>
2020-09-10 09:33:47 -0700

    First Commit

---
+++ statusHider/AppDelegate.swift
@@ -0,0 +1,67 @@
+//
+//  AppDelegate.swift
+//  statusHider
+//
+//  Created by Mehdi Ghannadan on 9/10/20.
+//  Copyright © 2020 Mehdi Ghannadan. All rights reserved.
+//
+
+import Cocoa
+import SwiftUI
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    var popover: NSPopover!
+    var statusBarItem: NSStatusItem!
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = ContentView()
+
+        // Create the window and set the content view.
+        
+        // Create the popover
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 400, height: 400)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        self.popover = popover
+        
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        
+        if let button = self.statusBarItem.button {
+             button.image = NSImage(named: "Icon")
+             button.action = #selector(togglePopover(_:))
+        }
+        
+
+    }
+    
+    // Create the status item
+    @objc func togglePopover(_ sender: AnyObject?) {
+         if let button = self.statusBarItem.button {
+              if self.popover.isShown {
+                   self.popover.performClose(sender)
+              } else {
+                   self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+              }
+         }
+    }
+    
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
+
+struct AppDelegate_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
---
+++ statusHider/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  statusHider
+//
+//  Created by Mehdi Ghannadan on 9/10/20.
+//  Copyright © 2020 Mehdi Ghannadan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
